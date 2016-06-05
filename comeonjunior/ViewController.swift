021409d2d97bf6f5e957988f9dc70837ab3254b7
kayazhou <he.zhou@gmail.com>
2016-06-04 22:08:52 -0500

    Initial Commit

---
+++ comeonjunior/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  comeonjunior
+//
+//  Created by kaisya on 6/4/16.
+//  Copyright © 2016 com.kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
